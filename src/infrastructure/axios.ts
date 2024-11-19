721b8103fc70c75624b23b49f07aec8a8fe13605
Davi Freire <fsdavi.dev@gmail.com>
2024-11-19 10:29:05 -0300

    Initial commit

---
+++ src/infrastructure/axios.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'localhost:3000:5238/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export default axiosInstance;
