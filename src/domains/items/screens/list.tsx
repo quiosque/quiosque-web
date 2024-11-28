daf293370239ea19c2303838d9a16a821f38a8bf
Davi Freire <fsdavi.dev@gmail.com>
2024-11-27 23:04:31 -0300

    Populate Data Table with Items

--- src/domains/items/screens/list.tsx
+++ src/domains/items/screens/list.tsx
@@ -1,29 +1,16 @@
-import React from "react"
-import { Payment, columns } from "../utils/columns"
-import { DataTable } from "../components/DataTable"
-
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ]
-}
+import React from "react";
+import { columns } from "../utils/columns";
+import { DataTable } from "../components/DataTable";
+import { useItems } from "../hooks";
 
 function ListItemsScreen() {
-  const data = getData()
+  const { data, isLoading } = useItems();
 
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={data} isLoading={isLoading} />
     </div>
-  )
-
+  );
 }
 
-export default ListItemsScreen
+export default ListItemsScreen;
