ef2d580671edd19cf71c657d5baab3cfba27dd35
Davi Freire <fsdavi.dev@gmail.com>
2024-12-04 23:38:54 -0300

    Implements delete item behaviour at list items table

--- src/domains/items/hooks/useItems.ts
+++ src/domains/items/hooks/useItems.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getItems } from "../services";
 
 function useItems() {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, refetch } = useQuery({
     queryKey: ["items"],
     queryFn: () => getItems(),
     refetchInterval: 10000,
@@ -11,6 +11,7 @@ function useItems() {
   return {
     data: data ?? [],
     isLoading,
+    refetch
   };
 }
 
