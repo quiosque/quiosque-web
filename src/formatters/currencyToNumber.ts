9f8da8c61f633bb2f6b277a4fd761b8e6c3cf0b8
Davi Freire <fsdavi.dev@gmail.com>
2025-01-16 12:00:55 -0300

    Env variable adjustment and remove unnecessary console.logs

--- src/formatters/currencyToNumber.ts
+++ src/formatters/currencyToNumber.ts
@@ -3,7 +3,6 @@ const currencyToNumber = (formattedValue: string): number => {
     .replace(/[^\d,.-]/g, "")
     .replace(",", ".");
 
-    console.log(numericValue)
   return parseFloat(numericValue);
 };
 
